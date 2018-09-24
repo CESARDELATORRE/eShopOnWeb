e76c4e5d788646264029dd592011d37ef9a2e090
franperezlopez <1222398+franperezlopez@users.noreply.github.com>
2018-09-24 17:48:45 +0200

    add cesar user

--- src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
+++ src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -13,6 +13,8 @@ namespace Microsoft.eShopWeb.Infrastructure.Identity
             var ankit = new ApplicationUser { UserName = "ankit@microsoft.com", Email = "ankit@microsoft.com", Id = "6e892e44-1c04-4e3b-b55e-fb75a95831fa" };
             await userManager.CreateAsync(ankit, "Pass@word1");
 
+            var cesar = new ApplicationUser { UserName = "cesardl@microsoft.com", Email = "cesardl@microsoft.com", Id = "71013715-676a-4a6b-91d3-7faf136e27a6" };
+            await userManager.CreateAsync(cesar, "Pass@word1");
         }
     }
 }
